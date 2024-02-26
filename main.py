faa0d2c51c7ee55562b953f4724127652be89371
sashabodiul <sashabodiul07@gmail.com>
2024-02-26 03:02:21 +0200

    Fixed bugs with download json and excel

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from fastapi import FastAPI, Request, Form, File
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import FastAPI, Request, Form, Query
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import asyncio
-import aiofiles
 import json
-from openpyxl import Workbook
-from req_script import get_urls, extract_data
+import pandas as pd
+from tempfile import NamedTemporaryFile
+from req_script import get_urls
 
 app = FastAPI()
 
@@ -16,41 +15,27 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Подключаем папку с шаблонами Jinja2
 templates = Jinja2Templates(directory="templates")
 
-#Функция запуска внешнего скрипта
-async def run_external_script(script_path):
-    process = await asyncio.create_subprocess_exec(
-        "python", script_path,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return stdout.decode(), stderr.decode()
-
 # Сохранение данных в JSON файл
 async def save_json(data):
-    async with aiofiles.open("static/data.json", mode="w") as f:
-        await f.write(json.dumps(data, ensure_ascii=False, indent=4))
+    return json.dumps(data, ensure_ascii=False, indent=4)
 
 # Сохранение данных в Excel файл
 async def save_excel(data):
-    wb = Workbook()
-    ws = wb.active
-    # Записываем заголовки
-    headers = list(data[0].keys())
-    ws.append(headers)
-    # Записываем данные
-    for row in data:
-        row_data = [str(value) if not isinstance(value, dict) else str(value) for value in row.values()]
-        ws.append(row_data)
-    wb.save("static/data.xlsx")
+    # Преобразование JSON в таблицу pandas DataFrame
+    data_frames = []
+    for block in data:
+        data_frames.append(pd.DataFrame.from_dict(block, orient='index'))
 
+    # Объединение DataFrame'ов и сохранение в Excel
+    result = pd.concat(data_frames)
+    result.to_excel('output.xlsx', index=False)
 
-#Страница формы ввода
+# Страница формы ввода
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-#Обработчик после отправки формы
+# Обработчик после отправки формы
 @app.post("/run_script/")
 async def run_script(request: Request, search_query: str = Form(...), search_location: str = Form(...)):
     # Получаем значения из HTML-инпутов
@@ -61,14 +46,28 @@ async def run_script(request: Request, search_query: str = Form(...), search_loc
     result = await get_urls(what, where) if where is not None else await get_urls(what)
 
     # Сохраняем данные в JSON и Excel
-    await save_json(result)
+    json_data = await save_json(result)
     await save_excel(result)
-
+    
     # Возвращаем HTML с результатами и ссылками на файлы для скачивания
     return templates.TemplateResponse(
         "result.html",
         {
             "request": request,
-            "data": result
+            "data": result,
+            "json_data": json_data
         }
     )
+
+# Обработчик для скачивания JSON файла
+@app.get("/download_json/")
+async def download_json(json_data: str = Query(...)):
+    temp_file = NamedTemporaryFile(delete=False, suffix=".json")
+    temp_file.write(json_data.encode())
+    temp_file.close()
+    return FileResponse(temp_file.name, media_type="application/json", filename="data.json")
+
+# Обработчик для скачивания Excel файла
+@app.get("/download_excel/")
+async def download_excel():
+    return FileResponse("output.xlsx", media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename="output.xlsx")
