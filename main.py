164a70a745f824d9611e415950c7fb18fe3d2441
sashabodiul <sashabodiul07@gmail.com>
2024-03-04 06:48:45 +0200

    fixed bags with output 50 files

--- main.py
+++ main.py
@@ -19,16 +19,27 @@ templates = Jinja2Templates(directory="templates")
 async def save_json(data):
     return json.dumps(data, ensure_ascii=False, indent=4)
 
-# Сохранение данных в Excel файл
-async def save_excel(data):
-    # Преобразование JSON в таблицу pandas DataFrame
-    data_frames = []
-    for block in data:
-        data_frames.append(pd.DataFrame.from_dict(block, orient='index'))
 
-    # Объединение DataFrame'ов и сохранение в Excel
-    result = pd.concat(data_frames)
-    result.to_excel('output.xlsx', index=False)
+async def save_excel(data):
+    # Создайте DataFrame из списка словарей
+    # Создайте DataFrame из списка словарей
+    df = pd.DataFrame(data)
+
+    # Укажите новые заголовки
+    new_columns = ['img', 'name', 'address', 'company_info', 'video', 'rating', 'portrait', 'contact']
+
+    # Переименуйте столбцы
+    df.columns = new_columns
+
+    # Замените все значения None на пустые значения в DataFrame
+    df = df.where(pd.notnull(df), None)
+
+    # Отсортируйте DataFrame по столбцу 'name'
+    df = df.sort_values(by='name')
+
+    # Сохраните DataFrame в файл Excel
+    df.to_excel('data.xlsx', index=False)
+
 
 # Страница формы ввода
 @app.get("/", response_class=HTMLResponse)
@@ -39,13 +50,23 @@ async def home(request: Request):
 @app.post("/run_script/")
 async def run_script(request: Request, search_query: str = Form(...), search_location: str = Form(...)):
     # Получаем значения из HTML-инпутов
-    where = search_query
-    what = search_location
+    what = search_query
+    where = search_location
     
     # Получаем данные
     result = await get_urls(what, where) if where is not None else await get_urls(what)
+    if where is not None and what is not None:
+        result = await get_urls(what, where)
+    if what is not None and where is None:
+        result = await get_urls(what)
     if not result:
         return templates.TemplateResponse('empty.html', {"request": request})
+    with open('result.json', 'w') as file:
+        # Assuming result is a list
+        result_str = json.dumps(result)
+
+        # Now you can write result_str to the file
+        file.write(result_str)
     # Сохраняем данные в JSON и Excel
     json_data = await save_json(result)
     await save_excel(result)
@@ -71,4 +92,4 @@ async def download_json(json_data: str = Query(...)):
 # Обработчик для скачивания Excel файла
 @app.get("/download_excel/")
 async def download_excel():
-    return FileResponse("output.xlsx", media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename="output.xlsx")
+    return FileResponse("data.xlsx", media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename="data.xlsx")
--- req_script.py
+++ req_script.py
@@ -3,82 +3,84 @@ import asyncio
 from bs4 import BeautifulSoup
 import re
 
-async def extract_data(url:str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            html_content = await response.text()
+global_container_data = []
+
+async def get_data_from_container(container):
+    bottom_links = container.find("ul", class_="bottom-links")
+    links = bottom_links.find_all('a')
+
+    # Rating logic
+    rating_element = container.find('span', class_='rating')
+    rating = None
+    if rating_element:
+        rating_class = rating_element.get('class', [])
+        for class_name in rating_class:
+            if class_name.startswith('perc-'):
+                rating_value = int(class_name.split('-')[1].split('-')[0]) // 20
+                rating = rating_value if rating_value > 0 else 1
+                break
+
+    global_container_data.append({
+        "img": (lambda x: x.get('src') if x else None)(container.find("img")),
+        "name": (lambda x: x.text if x else None)(container.find("h2")),
+        "address": {
+            "street": (lambda x: x.text if x else None)(container.find('span', itemprop="streetAddress")),
+            "postalCode": (lambda x: x.text if x else None)(container.find('span', itemprop="postalCode")),
+            "addressLocality": (lambda x: x.text if x else None)(container.find('span', itemprop="addressLocality"))
+        },
+        "company_info": (lambda x: x.get('href') if x else None)(container.find('a', class_="bg-light-blue")),
+        "video": (lambda x: x.get('href') if x else None)(container.find('a', class_="bg-orange")),
+        "rating": rating,
+        "portrait": (lambda x: x.get('href') if x else None)(links[0]),
+        "contact": (lambda x: x.get('href') if x else None)(links[1]),
+    })
+
+
+async def extract_data(url:str, session):
+    async with session.get(url) as response:
+        html_content = await response.text()
+
     soup = BeautifulSoup(html_content, 'html.parser')
-    container_data = {}
     containers = soup.find_all('div', class_='result-content')
     limit = soup.find('div', class_='info')
     limit = limit.find('strong').text
     match = re.search(r'\d+', str(limit))
     if match:
         limit = int(match.group())
-    counter = 0
+
     if containers:
-        for container in containers[:limit+1]:
-            counter += 1
-            bottom_links = container.find("ul", class_="bottom-links")
-            links = bottom_links.find_all('a')
+        tasks = []
+        for container in containers[:limit]:
+            tasks.append(get_data_from_container(container))
 
-            # Rating logic
-            rating_element = container.find('span', class_='rating')
-            rating = None
-            if rating_element:
-                rating_class = rating_element.get('class', [])
-                for class_name in rating_class:
-                    if class_name.startswith('perc-'):
-                        rating_value = int(class_name.split('-')[1].split('-')[0]) // 20
-                        rating = rating_value if rating_value > 0 else 1
-                        break
-
-            # Using lambda functions with checks for existence
-            container_data[counter] = {
-                "img": (lambda x: x.get('src') if x else None)(container.find("img")),
-                "name": (lambda x: x.text if x else None)(container.find("h2")),
-                "address": {
-                    "street": (lambda x: x.text if x else None)(container.find('span', itemprop="streetAddress")),
-                    "postalCode": (lambda x: x.text if x else None)(container.find('span', itemprop="postalCode")),
-                    "addressLocality": (lambda x: x.text if x else None)(container.find('span', itemprop="addressLocality"))
-                },
-                "company_info": (lambda x: x.get('href') if x else None)(container.find('a', class_="bg-light-blue")),
-                "video": (lambda x: x.get('href') if x else None)(container.find('a', class_="bg-orange")),
-                "rating": rating,
-                "portrait": (lambda x: x.get('href') if x else None)(links[0]),
-                "contact": (lambda x: x.get('href') if x else None)(links[1]),
-            }
-
-        return container_data
-    else:
-        return None
+        await asyncio.gather(*tasks)
 
 
 async def get_urls(what: str, where: str = None):
-    if where is not None:
-        url = f'https://www.firmenabc.at/result.aspx?what={what}&where={where}'
-    else:
-        url = f'https://www.firmenabc.at/suchbegriff/{what}'
-
+    url = f'https://www.firmenabc.at/result.aspx?what={what}&where={where}&exact=false&inTitleOnly=false&l=&si='
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             html_content = await response.text()
 
-    soup = BeautifulSoup(html_content, 'html.parser')
-    nav_pagination = soup.find('nav', class_='nav-pagination')
+        soup = BeautifulSoup(html_content, 'html.parser')
+        nav_pagination = soup.find('nav', class_='nav-pagination')
+        if nav_pagination:
+            limit_element = soup.find('div', class_='info').find('strong')
+            if limit_element:
+                limit_text = limit_element.text
+                limit = int(re.search(r'\d+', limit_text).group())  # Extract numeric part
+                pages = (limit // 50) + 1  # Calculate total pages to scrape
+                tasks = []
+                for i in range(pages):
+                    updated_url = f'{url}&p={i + 1}'  # Adjust URL for pagination
+                    tasks.append(extract_data(updated_url, session))
 
-    if nav_pagination:
-        links = nav_pagination.find_all('a')
-        last_href = links[-2].get('href')
-        match = re.search(r'\d+', str(last_href))
-        if match:
-            number = int(match.group())
-            clear_url = re.sub(r'\d+', '', str(last_href))
-            result = []
-            for i in range(1, number+1):
-                data = await extract_data(clear_url + str(i))  # Исправлено здесь
-                result.append(data)
-    else:
-        result = await extract_data(url)
+                await asyncio.gather(*tasks)
 
-    return result
+        else:
+            await extract_data(url, session)
+
+    global_result = global_container_data.copy()
+    global_container_data.clear()
+    return global_result
+
